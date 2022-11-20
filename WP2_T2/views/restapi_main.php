92de13aa91be01d68c2ca6af1bc8c3803c6ed37c
csabaialbert <68478226+csabaialbert@users.noreply.github.com>
2022-11-20 11:01:40 +0100

    restapi_main

--- WP2_T2/views/restapi_main.php
+++ WP2_T2/views/restapi_main.php
@@ -1,5 +1,7 @@
 <h2>
-    <br>A következő címen és telefonon érhet el:<br>
-    <br>Cím: ...<br>
-    <br>Telefon: ...<br>
+    <br>A honlapunkon két REST API-t felhasználó lap található.<br>
 </h2>
+<p>
+   Az elsőnél a "https://gorest.co.in/public-api/users" címen megtalálható API-t tudja a felhasználó tesztelni, egy az erre elkészített tesztoldallal.
+    <br><br>A második oldalon pedig a tanösvények adatbázisból tud adatokat lekérdezni a fentebb említett REST API segítségével.
+</p>
