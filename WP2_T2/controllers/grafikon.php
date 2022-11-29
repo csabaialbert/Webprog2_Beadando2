d9d5a85bc212f3eff6fd1a99bcbcec7a18415059
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-11-29 02:35:33 +0100

    Controller and view for graph

--- WP2_T2/controllers/grafikon.php
+++ WP2_T2/controllers/grafikon.php
@@ -3,10 +3,15 @@
 class Grafikon_Controller {
 	public $baseName = 'grafikon';  //meghatározni, hogy melyik oldalon vagyunk
 
-	public function main(array $vars) // a router által továbbított paramétereket kapja
-	{
+	public function main(array $vars) { // a router által továbbított paramétereket kapja
+		$osszTanosvenyModel = new Ossztanosveny_Model;
+		//a modellben belépteti a felhasználót
+		$retData = $osszTanosvenyModel->get_data($vars);
+
 		//betöltjük a nézetet
 		$view = new View_Loader($this->baseName . "_main");
+		foreach ($retData as $name => $value)
+			$view->assign($name, $value);
 	}
 }
 
--- WP2_T2/views/grafikon_main.php
+++ WP2_T2/views/grafikon_main.php
@@ -1,4 +1,60 @@
-<h2>Grafikon</h2>
-<div style="width: 500px;"><canvas id="park-chart"></canvas></div><br/>
+<div style="width: 600px; height: 400px; margin: auto;">
+    <canvas id="myChart"></canvas>
+</div>
 
-<script type="module" src="../js/park_chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    // Setup Block
+    const park_names = <?php echo json_encode($viewData['uzenet']['park']) ?>;
+    const park_count = <?php echo json_encode($viewData['uzenet']['darab']) ?>;
+    const data = {
+        labels: park_names,
+        datasets: [{
+            label: 'Tanösvények száma nemzeti parkonként',
+            data: park_count,
+            borderWidth: 1,
+            backgroundColor: '#FF6363CC',
+        }]
+    };
+
+    // Config Block
+    const config = {
+        type: 'bar',
+        data,
+        options: {
+            plugins: {  // 'legend' now within object 'plugins {}'
+                legend: {
+                    title: {
+                        padding: 10
+                    },
+                    labels: {
+                        color: '#F5DEB3FF',
+                        font: {
+                            size: 18
+                        }
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    ticks: {
+                        color: '#F5DEB3FF',
+                    },
+                    beginAtZero: true,
+                },
+                x: {
+                    ticks: {
+                        color: '#F5DEB3FF',
+                    }
+                }
+            }
+        },
+    }
+
+    // Render Block
+    const myChart = new Chart(
+        $('#myChart'),
+        config
+    );
+</script>
